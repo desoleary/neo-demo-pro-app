84db412e3d2d9c78f3198704fedd42dd50bb1c47
desoleary <desoleary@users.noreply.github.com>
2025-06-06 18:48:17 -0600

    feat: update observability hooks and accounts collection

--- packages/accounts-service/src/graphql/resolvers/user.ts
+++ packages/accounts-service/src/graphql/resolvers/user.ts
@@ -7,19 +7,25 @@ interface UserDoc {
 }
 
 export function createResolvers(db: Db) {
-  const users = db.collection<UserDoc>('users');
+  const users = db.collection<UserDoc>('accounts_users');
   return {
     Query: {
       async user(_: any, { id }: { id: string }) {
         const user = await users.findOne({ _id: new ObjectId(id) });
-        return user ? { id: user._id.toHexString(), email: user.email, name: user.name } : null;
+        return user
+          ? { id: user._id.toHexString(), email: user.email, name: user.name }
+          : null;
       },
     },
     User: {
       __resolveReference(ref: { id: string }) {
-        return users.findOne({ _id: new ObjectId(ref.id) }).then((u) =>
-          u ? { id: u._id.toHexString(), email: u.email, name: u.name } : null
-        );
+        return users
+          .findOne({ _id: new ObjectId(ref.id) })
+          .then((u) =>
+            u
+              ? { id: u._id.toHexString(), email: u.email, name: u.name }
+              : null,
+          );
       },
     },
   };
